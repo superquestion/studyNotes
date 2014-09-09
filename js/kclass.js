5ee0a78efacfacc19859e4883ada870d65b960b7
pan.yang <pan.yang@ttpod.com>
2014-09-09 11:23:53 +0800

    add fis demo

--- js/kclass.js
+++ js/kclass.js
@@ -1,63 +1,78 @@
-var klass = function (Parent, props) {
+(function() {
+        var initializing = false;
 
-    var Child, F, i;
-    //uber属性，这个属性直接指向父对象的prototype属性
-   //如果child存在，并且有construct,则调用child的父类构造函数
-    Child = function () {
-        if (Child.uber && Child.uber.hasOwnProperty("__construct")) {
-            Child.uber.__construct.apply(this, arguments);
-        }
-        //如果child有构造函数，则调用child的construct
-        if (Child.prototype.hasOwnProperty("__construct")) {
-            Child.prototype.__construct.apply(this, arguments);
-        }
-    };
-    Parent = Parent || Object;
-    //创建一个空函数
-    F = function () {
-    };
-    F.prototype = Parent.prototype;
-    Child.prototype = new F();
-    Child.uber = Parent.prototype;
-    Child.prototype.constructor = Child;
-    //拷贝method
-    for (i in props) {
-        if (props.hasOwnProperty(i)) {
-            Child.prototype[i] = props[i];
-        }
-    }
-    return Child;
-};
+        function kClass(baseClass, prop) {
 
-var Man = klass(null, {
-    __construct: function (what) {
-        console.log("Man's constructor");
-        this.name = what;
-    },
-    getName: function () {
-        return this.name;
-    }
-});
+            // 只接受一个参数的情况 - jClass(prop)
 
-var first = new Man('Adam'); // logs "Man's constructor"
-console.log(first.getName()); // "Adam"
+            if (typeof(baseClass) === "object") {
 
-var SuperMan = klass(Man, {
-    __construct: function (what,age) {
-        console.log("SuperMan's constructor");
-        this.age = age;
-    },
-    getName: function () {
-        var name = SuperMan.uber.getName.call(this);
-        return "I am " + name;
-    },
-    getAge:function(){
-        return this.age;
-    }
-});
+                prop = baseClass;
 
-var clark = new SuperMan('Clark Kent',12);
-console.log(clark.getName()); // "I am Clark Kent"
-console.log(clark.getAge())  // 12
-console.log(clark instanceof Man); // true
-console.log(clark instanceof SuperMan); // true
+                baseClass = null;
+
+            }
+
+            // 本次调用所创建的类（构造函数）
+
+            function F() {
+
+
+                // 这就提供了在实例对象中调用父类方法的途径
+                //储存base的原型
+                if (baseClass) {
+
+                    this.baseprototype = baseClass.prototype;
+
+                }
+                // 如果当前处于实例化类的阶段，则调用init原型函数
+                if (!initializing) {
+                    this.init.apply(this, arguments);
+
+                }
+
+            }
+
+            // 如果此类需要从其它类扩展
+
+            if (baseClass) {
+
+
+                initializing = true;
+
+                //继承基类
+                F.prototype = new baseClass();
+
+                F.prototype.constructor = F;
+
+                initializing = false;
+
+            }
+
+            // 覆盖父类的同名函数
+
+            for (var name in prop) {
+
+                if (prop.hasOwnProperty(name)) {
+                    //当基类存在，并且子类存在和父类同名的方法
+                    if (baseClass && typeof(prop[name]) === "function" && typeof(F.prototype[name]) === "function") {
+                        F.prototype[name] = (function(name, fn) {
+                            return function() {
+                                this.base = baseClass.prototype[name];
+                                return fn.apply(this, arguments);
+                            }
+                        })(name, prop[name])
+                    } else {
+                        F.prototype[name] = prop[name];
+                    }
+
+
+
+                }
+
+            }
+
+            return F;
+
+        };
+    }()
